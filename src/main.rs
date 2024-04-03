b58d69d57bc9d17c9f7b7f4e5125ecd714146290
guilhermegcaires <guilhermegcaires@gmail.com>
2024-04-03 11:28:38 -0300

    Reading bytes offset

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use std::{fs::File, io::Read};
+use std::{
+    fs::File,
+    io::{Cursor, Read, Seek, SeekFrom, Write},
+};
+
+use byteorder::{LittleEndian, ReadBytesExt};
 
 fn main() {
     let mut file = match File::open("./replays/7656819028.dem") {
         Ok(file) => file,
         Err(_) => panic!("Failed to open the file."),
     };
-    let mut buffer = [0; 12];
-    match file.read_exact(&mut buffer) {
+    let mut header = [0; 8];
+    match file.read_exact(&mut header) {
         Ok(_) => {
-            let first_12_bytes = String::from_utf8_lossy(&buffer);
-            println!("First 12 bytes: {}", first_12_bytes);
+            let header = String::from_utf8_lossy(&header);
+            println!("Header: {}", header);
         }
         Err(_) => {
             panic!("Failed to read the file.");
+        }
+    }
+
+    let mut offset_bytes = [0; 4];
+
+    match file.read_exact(&mut offset_bytes) {
+        Ok(_) => {
+            let mut offset = Cursor::new(offset_bytes);
+
+            let offset_value = offset.read_u32::<LittleEndian>().unwrap();
+            println!("Offset: {:?}", offset_value);
+        }
+        Err(_) => {
+            panic!("Failed to read the offset bytes.");
         }
     }
 }
